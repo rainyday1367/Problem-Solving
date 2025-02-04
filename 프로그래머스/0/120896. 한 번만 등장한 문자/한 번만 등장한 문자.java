e6ee4f252b855a0d30cd879ad0c92e634d057b6c
Donghan <98398484+rainyday1367@users.noreply.github.com>
2025-02-04 13:54:47 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 25.34 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int[] alpha = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            alpha[(int)(s.charAt(i) - 'a')]++;
+        }
+        for (int i = 0; i < alpha.length; i++) {
+            if (alpha[i] == 1){
+                answer += (char)(i + 'a');
+            }
+        }
+        return answer;
+    }
+}
