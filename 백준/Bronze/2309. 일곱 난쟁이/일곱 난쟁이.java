09ce3c7380c01833360dbd483294a65adff02ca3
Donghan <98398484+rainyday1367@users.noreply.github.com>
2025-02-20 00:43:40 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[9];
+        int[] result = new int[7];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+        int index1 = 0, index2 = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    index1 = i;
+                    index2 = j;
+                }
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (i == index1 || i == index2) {
+                continue;
+            }
+            sb.append(arr[i] + "\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
