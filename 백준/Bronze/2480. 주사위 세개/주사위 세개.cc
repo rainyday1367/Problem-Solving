5fbc467fd081b0f93ea5dfebe40540e32baf9609
Donghan <98398484+rainyday1367@users.noreply.github.com>
2025-09-14 16:31:44 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, n2, n3;
+
+int cal(int n ,int n2, int n3) {
+    int m = 0;
+
+    if (n == n2 && n2 == n3 && n == n3) {
+        m = 10000 + n * 1000;
+    }
+    else if (n == n2 || n == n3) {
+        m = 1000 + n * 100;
+    }
+    else if (n2 == n3) {
+        m = 1000 + n2 * 100;
+    }
+    else {
+        int mx = max(n, n2);
+        mx = max(mx, n3);
+
+        m = mx * 100;
+    }
+    return m;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> n2 >> n3;
+    int m = cal(n, n2, n3);
+    cout << m << '\n';
+    return 0;
+}
